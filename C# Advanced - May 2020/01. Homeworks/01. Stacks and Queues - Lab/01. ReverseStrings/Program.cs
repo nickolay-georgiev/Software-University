98a3e6479369c08bae3571ef36f2324f9a8a81f4
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-07-27 16:24:43 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/01. Stacks and Queues - Lab/01. ReverseStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            Stack<string> input = new Stack<string>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                input.Push(data[i].ToString());
+            }
+
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
