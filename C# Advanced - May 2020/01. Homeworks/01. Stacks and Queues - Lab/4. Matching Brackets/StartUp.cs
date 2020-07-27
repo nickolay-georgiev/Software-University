1f9b55d84ec8cdf0f01adee6c33cd842ee1aa9c5
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-07-27 19:46:55 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/01. Stacks and Queues - Lab/4. Matching Brackets/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    int startIndex = i;
+                    indexes.Push(startIndex);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    string sub = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(sub);
+                }
+            }
+        }
+    }
+}
