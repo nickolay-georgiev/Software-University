ea4f7b855ae0b7163ebfa4e9e82cfae42f23edd0
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-07-27 16:44:15 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/01. Stacks and Queues - Lab/3. Simple Calculator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<string> numbers = new Stack<string>(input.Reverse());
+
+            while (numbers.Count > 1)
+            {
+                int firstNumber = int.Parse(numbers.Pop());
+                string @operator = numbers.Pop();
+                int secondNumber = int.Parse(numbers.Pop());
+
+                switch (@operator)
+                {
+                    case "+":
+                        numbers.Push((firstNumber + secondNumber).ToString());
+                        break;
+
+                    case "-":
+                        numbers.Push((firstNumber - secondNumber).ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(numbers.Pop().ToString());
+        }
+    }
+}
