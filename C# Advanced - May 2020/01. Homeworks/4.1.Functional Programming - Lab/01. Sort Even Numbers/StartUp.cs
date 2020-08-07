59dcdba489161bb27f5b7162cbd67e5afbf4aa39
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-07 10:18:23 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/4.1.Functional Programming - Lab/01. Sort Even Numbers/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, bool> func = x => x % 2 == 0;
+
+            var filteredNumbers = input.Where(func).ToList();
+            filteredNumbers.Sort();
+
+            Console.WriteLine(string.Join(", ", filteredNumbers));
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/4.1.Functional Programming - Lab/02. Sum Numbers/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
