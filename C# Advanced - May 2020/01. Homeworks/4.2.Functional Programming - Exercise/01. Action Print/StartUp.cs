2d2f30eff53792185e8118def8b09c7208d446f0
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-09 10:04:49 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/4.2.Functional Programming - Exercise/01. Action Print/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            foreach (var name in input)
+            {
+                print(name);
+            }
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/4.2.Functional Programming - Exercise/02. Knights of Honor/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Action<string> print = x => Console.WriteLine($"Sir {x}");
+
+                foreach (var name in input)
+                {
+                    print(name);
+                }
+            }
+        }
+    }
+}
