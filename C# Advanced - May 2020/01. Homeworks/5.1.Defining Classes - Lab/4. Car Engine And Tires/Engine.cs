e5836acf23f9ab45f8546b13d4ac62b327986730
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-14 15:34:30 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/5.1.Defining Classes - Lab/4. Car Engine And Tires/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+		private int horsePower;
+		private int cubicCapacity;
+
+		public Engine(int horsePower, int cubicCapacity)
+		{
+			HorsePower = horsePower;
+			CubicCapacity = cubicCapacity;
+		}
+
+		public int HorsePower
+		{
+			get { return  horsePower; }
+			set {  horsePower = value; }
+		}
+
+		public int CubicCapacity
+		{
+			get { return cubicCapacity; }
+			set { cubicCapacity = value; }
+		}
+
+	}
+}
