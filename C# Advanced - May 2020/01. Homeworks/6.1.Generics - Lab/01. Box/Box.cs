8f0d49bc3d8cb4f785c86abe18f5ce0bda3b6fdc
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-19 15:10:08 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/6.1.Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> Store;
+
+        public int Count => this.Store.Count;
+
+        public Box()
+        {
+            this.Store = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.Store.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = this.Store[this.Store.Count - 1];
+            this.Store.RemoveAt(this.Store.Count - 1);
+
+            return element;
+        }
+    }
+}
