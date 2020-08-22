f77e0296cf58c7d3710b5301de2abf247ad33dac
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-22 15:44:55 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/6.2.Generics - Exercise/02. Generic Box of Integer/Generics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        public List<T> Elemets { get; set; }
+
+        public Box()
+        {
+            this.Elemets = new List<T>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var element in this.Elemets)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void Add(T element)
+        {
+            this.Elemets.Add(element);
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/6.2.Generics - Exercise/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var box = new Box<int>();
+                         
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
