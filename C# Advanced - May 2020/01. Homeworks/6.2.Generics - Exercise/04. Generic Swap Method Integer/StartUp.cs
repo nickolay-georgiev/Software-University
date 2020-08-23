fa39e00a94b52751d7925b5fa4a0dfd89f17038e
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-23 17:39:57 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/6.2.Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {                         
+            int lines = int.Parse(Console.ReadLine());
+
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                myList.Add(input);
+            }
+
+            int[] swapIndexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = swapIndexes[0];
+            int secondIndex = swapIndexes[1];
+
+            myList = MySwap(myList, firstIndex, secondIndex);
+
+            foreach (var element in myList)
+            {
+                Console.WriteLine($"{element.GetType()}: {element}");
+            }
+        }
+
+        public static List<T> MySwap<T>(List<T> elements, int firstIndex, int secondIndex)
+        {
+            var myTemp = elements[firstIndex];
+
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = myTemp;
+
+            return elements;
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/6.2.Generics - Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {                         
+            int lines = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            string element = Console.ReadLine();
+
+            int result = box.MyCompare(element);
+
+            Console.WriteLine(result);
+        }
+        
+        public static List<T> MySwap<T>(List<T> elements, int firstIndex, int secondIndex)
+        {
+            var myTemp = elements[firstIndex];
+
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = myTemp;
+
+            return elements;
+        }
+    }
+}
