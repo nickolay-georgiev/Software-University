a60477024d7e4f40d3056a51c789bb9ee35ecfce
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-26 18:10:30 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/7.1.Iterators and Comparators - Lab/01. Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> library { get; set; } 
+
+        public Library(params Book[] books)
+        {
+            this.library = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return this.library.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
