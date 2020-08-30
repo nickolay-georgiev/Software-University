8623cbc3cc83782aaa0811615ed0331faf4678a2
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-08-30 10:40:37 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/7.2.Iterators and Comparators - Exercise/03. Stack/CustomStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public CustomStack()
+        {
+            this.elements = new List<T>();
+        }
+
+        public void Push(List<T> elements)
+        {
+            foreach (var item in elements)
+            {
+                this.elements.Add(item);
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            else
+            {
+                this.elements.RemoveAt(this.elements.Count - 1);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.elements.Count - 1; i >= 0; i--)
+            {
+                yield return this.elements[i];
+            }
+
+            for (int i = this.elements.Count - 1; i >= 0; i--)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/7.2.Iterators and Comparators - Exercise/03. Stack/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            CustomStack<string> test = new CustomStack<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command.Equals("END"))
+                {
+                    break;
+                }
+
+                string[] data = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => x.TrimEnd(',')).ToArray();
+                string action = data[0];
+
+                try
+                {
+                    switch (action)
+                    {
+                        case "Push":
+
+                            List<string> elementsToPush = data.Skip(1).ToList();
+                            test.Push(elementsToPush);
+                            break;
+
+                        case "Pop":
+                            test.Pop();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
