ea8ea69ff65d0eb74dc0027320eff8af53f3a187
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-01 13:28:55 +0300

    Done 100/100

---
+++ C# Advanced - May 2020/01. Homeworks/7.2.Iterators and Comparators - Exercise/06. Equality Logic/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if(this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+
+            if(this.Age == person.Age && this.Name == person.Name)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+    }
+}
---
+++ C# Advanced - May 2020/01. Homeworks/7.2.Iterators and Comparators - Exercise/06. Equality Logic/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                
+                sortedSet.Add(new Person(name, age));
+                hashSet.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(sortedSet.Count);           
+            Console.WriteLine(hashSet.Count);           
+        }
+    }
+}
