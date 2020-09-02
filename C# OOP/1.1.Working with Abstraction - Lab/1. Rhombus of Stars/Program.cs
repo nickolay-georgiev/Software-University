2458808cdce926744903a196a80e8815f564492c
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-02 17:02:07 +0300

    Done 100/100

---
+++ C# OOP/1.1.Working with Abstraction - Lab/1. Rhombus of Stars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1._Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                PrintRow(n, i);
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                PrintRow(n, i);
+            }
+        }
+
+        private static void PrintRow(int n, int i)
+        {
+            for (int k = 0; k < n - i; k++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < i + 1; j++)
+            {
+                Console.Write("*" + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
