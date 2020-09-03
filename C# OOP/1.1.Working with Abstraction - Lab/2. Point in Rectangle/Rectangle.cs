fddc83ef6134fbc90094b8df9550501ba450343d
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-03 11:47:20 +0300

    Done 100/100

---
+++ C# OOP/1.1.Working with Abstraction - Lab/2. Point in Rectangle/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            if (this.TopLeft.X <= point.X && point.X <= this.BottomRight.X && this.TopLeft.Y <= point.Y && this.BottomRight.Y >= point.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# OOP/1.1.Working with Abstraction - Lab/2. Point in Rectangle/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Abstractions
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] rectangleData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int topLeftX = rectangleData[0];
+            int topLeftY = rectangleData[1];
+            Point leftXY = new Point(topLeftX, topLeftY);
+
+            int bottomRightX = rectangleData[2];
+            int bottomRightY = rectangleData[3];
+            Point rightXY = new Point(bottomRightX, bottomRightY);
+
+            Rectangle rectangle = new Rectangle(leftXY, rightXY);
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                int[] coordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int x = coordinates[0];
+                int y = coordinates[1];
+
+                Console.WriteLine(rectangle.Contains(new Point(x, y)));
+            }
+
+        }
+    }
+}
