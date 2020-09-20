13c4ce402fa7fca7f7e3c99a22abf4f03b12eb0f
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-20 12:13:28 +0300

    Done 100/100

---
+++ C# OOP/3.2.Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(this.Length)} cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(this.Width)} cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(this.Height)} cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return this.Length * this.Height * 2 + this.Width * this.Height * 2;
+        }
+
+        public double GetVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {this.GetSurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.GetVolume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/3.2.Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            try
+            {
+
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine(box);
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+    }
+}
