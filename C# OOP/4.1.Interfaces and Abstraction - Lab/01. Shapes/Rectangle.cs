ec9819d7bcadacf83ceb2ca343bcae563d4da607
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-23 08:40:56 +0300

    Done 100/100

---
+++ C# OOP/4.1.Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public int Width
+        {
+            get { return Width; }
+            private set { Width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+
+            for (int i = 0; i < this.Height - 2; i++)
+            {
+                Console.WriteLine(new string('*', 1) + new string(' ', this.Width - 2) + new string('*', 1));
+            }
+
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
