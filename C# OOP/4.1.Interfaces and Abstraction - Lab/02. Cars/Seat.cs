73d76f4f79eb248df2486d9bf7ec81b687d5d799
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-09-24 09:25:36 +0300

    Done 100/100

---
+++ C# OOP/4.1.Interfaces and Abstraction - Lab/02. Cars/Seat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model}");
+            sb.AppendLine("Engine start");
+            sb.AppendLine("Breaaak!");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/4.1.Interfaces and Abstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set ; }
+        public string Color { get ; set; }
+        public int Battery { get ; set ; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine("Engine start");
+            sb.AppendLine("Breaaak!");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
