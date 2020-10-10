4f9b473c77a4ad33037caf5be600eb178f1dad4b
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-10 11:43:52 +0300

    Done 100/100

---
+++ C# OOP/6.1.Reflection and Attributes - Lab/03. Mission Private Impossible/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//namespace Stealer
+//{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+
+            string result = spy.RevealPrivateMethods("Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+//}
