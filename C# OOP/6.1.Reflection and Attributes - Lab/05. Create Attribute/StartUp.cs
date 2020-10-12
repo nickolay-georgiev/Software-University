0afd782a8ad50f5ef30e6082d9c0200205eb8211
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-12 13:19:49 +0300

    Done 100/100

---
+++ C# OOP/6.1.Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+//namespace Stealer
+//{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main()
+        {
+        }
+    }
+//}
