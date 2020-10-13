340ad5d9edb6fb922f214890e4b7f641ec06fe00
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-13 09:47:48 +0300

    Done 100/100

---
+++ C# OOP/6.1.Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+//namespace Stealer
+//{
+[Author("Ventsi")]
+public class StartUp
+{
+    [Author("Gosho")]
+    static void Main()
+    {
+        var tracker = new Tracker();
+
+        tracker.PrintMethodsByAuthor();
+    }
+}
+//}
---
+++ C# OOP/6.1.Reflection and Attributes - Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+//namespace Stealer
+//{
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        var type = typeof(StartUp);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attrs = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attr in attrs)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+            }
+        }
+    }
+}
+//}
