5bc5f13ef1d7ff5e53f242e16eb2ea2c777c2e3a
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-18 15:42:42 +0300

    Done 100/100

---
+++ C# OOP/7.1.Unit Testing - Lab/1.1. Test Axe/AxeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private Axe axe;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.axe = new Axe(5, 10);
+        }
+
+        [Test]
+        public void TestConstrucor()
+        {
+            int currentAttack = 5;
+            int expectedAttack = this.axe.AttackPoints;
+
+            Assert.That(expectedAttack == currentAttack);
+        }
+
+        [Test]
+        public void TestConstrucor1()
+        {
+            int currentAttack = 10;
+            int expectedAttack = this.axe.DurabilityPoints;
+
+            Assert.That(expectedAttack == currentAttack);
+        }
+
+        [Test]
+        public void ThrowsException()
+        {
+            var newAxe = new Axe(10, 0);
+
+            Assert.Throws<InvalidOperationException>(() => newAxe.Attack(new Dummy(50, 50)), "Axe is broken.");
+        }
+        [Test]
+        public void AttackMethod()
+        {
+            var dummy = new Dummy(10, 10);
+
+            this.axe.Attack(dummy);
+
+            Assert.That(this.axe.DurabilityPoints == 9);
+        }
+    }
+}
