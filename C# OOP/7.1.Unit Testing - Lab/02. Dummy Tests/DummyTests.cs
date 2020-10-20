ab3f0223012e9cfeaa666d73bb4add3c2e71d570
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-20 09:13:33 +0300

    Done 100/100

---
+++ C# OOP/7.1.Unit Testing - Lab/02. Dummy Tests/DummyTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private Dummy dummy;
+        [SetUp]
+        public void SetUp()
+        {
+            this.dummy = new Dummy(10, 10);
+        }
+
+        [Test]
+        public void TestConstructorHealth()
+        {
+            Assert.That(this.dummy.Health == 10);
+        }
+
+        [Test]
+        public void TestTakeAttackMethod()
+        {
+            var test = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => test.TakeAttack(10), "Dummy is dead.");
+        }
+
+        [Test]
+        public void TestTakeAttackMethod1()
+        {
+            var test = new Dummy(20, 10);
+
+            test.TakeAttack(10);
+
+            Assert.That(test.Health == 10);
+        }
+
+        [Test]
+        public void TestGiveExpMethod()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.dummy.GiveExperience(), "Target is not dead.");
+        }
+
+        [Test]
+        public void TestReturnExp()
+        {
+            var test = new Dummy(0, 10);
+
+            Assert.That(test.GiveExperience() == 10);
+        }
+
+        [Test]
+        public void TestReturnExp1()
+        {
+            var test = new Dummy(0, 10);
+
+            Assert.IsTrue(test.IsDead());
+        }
+    }
+}
