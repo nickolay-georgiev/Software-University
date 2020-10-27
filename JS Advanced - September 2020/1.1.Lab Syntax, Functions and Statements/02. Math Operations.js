4326f5d7f197afec7cd0cde5bd3655e5ae233929
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-10-27 14:40:34 +0200

    Rename folder

--- JS Advanced - September 2020/1.1.Lab Syntax, Functions and Statements/02. Math Operations.js
+++
@@ -1,7 +0,0 @@
-function solve(firstArg, secondArg, inputOperator) {
-
-    let result = eval(firstArg + inputOperator + secondArg);
-
-    console.log(result);
-    
-}
--- JS Advanced - September 2020/1.1.Lab Syntax, Functions and Statements/03. Sum of Numbers N…M.js
+++
@@ -1,8 +0,0 @@
-function solve(firstArg, secondArg) {
-    let sum = 0;
-
-    for (let index = Number(firstArg); index <= Number(secondArg); index++) {
-        sum += index;
-    }
-    console.log(sum);    
-}
