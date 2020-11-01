67242ea720cf94277865ceb07166d6d7dd31c4e7
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-11-01 17:32:55 +0200

    Done 100/100

---
+++ JS Advanced - September 2020/1.2.Exercise Syntax, Functions and Statements/01. Fruit.js
@@ -0,0 +1,3 @@
+function solve(firstArg, secondArg, thirdArg) {    
+    console.log(`I need $${(secondArg / 1000 * thirdArg).toFixed(2)} to buy ${(secondArg/1000).toFixed(2)} kilograms ${firstArg}.`);    
+}
---
+++ JS Advanced - September 2020/1.2.Exercise Syntax, Functions and Statements/02. Greatest Common Divisor – GCD.js
@@ -0,0 +1,12 @@
+function solve(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number'))
+        return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    console.log(x);
+}
