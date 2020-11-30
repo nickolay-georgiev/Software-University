41180b05fd3c893682cb2aefcf07ac77428ecbfa
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-11-30 22:27:54 +0200

    Done 100/100

---
+++ JS Advanced - September 2020/3.2.Exercise Objects & Classes/10. Length Limit.js
@@ -0,0 +1,25 @@
+class Stringer {
+    innerString;
+    innerLength;
+
+    constructor(srt, lenght) {
+        this.innerString = srt;
+        this.innerLength = lenght;
+    }
+
+    increase(length) {
+        this.innerLength += length;
+    }
+
+    decrease(length) {
+        this.innerLength = this.innerLength - length > 0 ? this.innerLength - length : 0;
+    }
+
+    toString() {
+        if (this.innerLength === 0) {
+            return '...';
+        }
+
+        return this.innerString.slice(0, this.innerLength) + (this.innerLength >= this.innerString.lenght ? '' : '...');
+    }
+}
