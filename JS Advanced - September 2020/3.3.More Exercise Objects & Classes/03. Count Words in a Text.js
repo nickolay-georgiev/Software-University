7b22fbef6e33ea06ceb59111cb71a869be706367
nickolay-georgiev <68684837+nickolay-georgiev@users.noreply.github.com>
2020-12-04 20:50:00 +0200

    Done 100/100

---
+++ JS Advanced - September 2020/3.3.More Exercise Objects & Classes/03. Count Words in a Text.js
@@ -0,0 +1,21 @@
+function solve(input) {
+
+    let data = input[0].split(/\W+/gm).filter(x => x != '')
+
+    let result = {};
+
+    for (let index = 0; index < data.length; index++) {
+
+        let currentWord = data[index];
+
+        if (!result[currentWord]) {
+
+            result[currentWord] = 1;
+        }
+        else {
+            result[currentWord]++;
+        }
+    }
+
+    console.log(JSON.stringify(result));
+}
